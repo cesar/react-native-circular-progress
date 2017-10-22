3c88cc85b85ddb100e382c0f572c38ceace1fe36
Cesar <cesarcruz91@gmail.com>
2017-10-21 17:12:06 -0700

    Android bug issue

--- src/CircularProgress.js
+++ src/CircularProgress.js
@@ -26,7 +26,7 @@ export default class CircularProgress extends React.Component {
 
   render() {
     const { size, width, tintColor, backgroundColor, style, rotation, linecap, children } = this.props;
-    const backgroundPath = this.circlePath(size / 2, size / 2, size / 2 - width / 2, 0, 360 * .9999);
+    const backgroundPath = this.circlePath(size / 2, size / 2, size / 2 - width / 2, 1, 360);
 
     const fill = this.extractFill(this.props.fill);
     const circlePath = this.circlePath(size / 2, size / 2, size / 2 - width / 2, 0, (360 * .9999) * fill / 100);
